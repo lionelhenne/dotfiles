7bcc4419ff5393c179d52eec3e96a1f754ed2358
Lionel Henne <lionelhenne@gmail.com>
2025-11-27 14:30:35 +0100

    fix: colors

--- .local/lib/setup/webdev.sh
+++ .local/lib/setup/webdev.sh
@@ -232,7 +232,7 @@ EOF
     echo -e "    ${SILVER}• Laravel Valet:${RESET} $(composer global show laravel/valet 2>/dev/null | grep 'versions' | awk '{print $NF}')"
     
     if command -v fnm >/dev/null 2>&1; then
-        echo -e "    ${SILVER}• Node.js:${RESET} $(node -v 2>/dev/null) ${SILVER}(via fnm $(fnm --version 2>/dev/null | awk '{print $2}'))${RESET}"
+        echo -e "    • Node.js:${RESET} $(node -v 2>/dev/null) ${SILVER}(via fnm ${RESET}$(fnm --version 2>/dev/null | awk '{print $2}')${SILVER})${RESET}"
         echo -e "    ${SILVER}• npm:${RESET} $(npm -v 2>/dev/null)"
     else
         echo -e "    ${SILVER}• Node.js:${RESET} not installed"
@@ -248,5 +248,4 @@ EOF
     if brew services list | grep -q "postgresql@18.*started"; then
         echo -e "    ${SILVER}• PostgreSQL:${RESET} running (postgres/postgres)"
     fi
-    echo
 }
